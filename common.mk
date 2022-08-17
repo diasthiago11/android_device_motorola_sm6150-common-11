dd67cdaf085c5a45d7e3ac8ae3c68a8b6360edc3
BarryBlackCat <everydayonechoice@gmail.com>
2022-08-17 16:00:20 +0000

    sm6150-common: Build soundtrigger 2.0 and 2.3
    libc    : Unable to set property ctl.interface_start to android.hardware.soundtrigger@2.0::ISoundTriggerHw/default: error code: 0x20
    hwservicemanager: Tried to start android.hardware.soundtrigger@2.0::ISoundTriggerHw/default as a lazy service, but was unable to. Usually this happens when a service is not installed, but if the service is intended to be used as a lazy service, then it may be configured incorrectly.
    I hwservicemanager: getTransport: Cannot find entry android.hardware.soundtrigger@2.3::ISoundTriggerHw/default in either framework or device VINTF manifest.

--- common.mk
+++ common.mk
@@ -117,7 +117,9 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
+    android.hardware.soundtrigger@2.0-impl \
     android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.3-impl \
 	sound_trigger.primary.sm6150 \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.a2dp.default \
